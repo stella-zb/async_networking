cb2de8c5df5b00317b6834d742efc851a0139efa
Ted Ogmundson <togmund@gmail.com>
2019-10-23 20:44:36 +0000

    update to the play.js interface, confirms whether connected (has not connected successfully so far) and preps for receiving ddata for when we actually connect

--- snake-client/play.js
+++ snake-client/play.js
@@ -8,15 +8,31 @@ const connect = () => {
     host: '192.168.99.149',
     port: 50541
   });
+  
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  // Verify we connected
+  conn.on('connect', () => {
+    console.log(`I'm In.`)
+})
+
+  // Receive messages from the server
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  // Not sure what this does!
   return conn;
 }
 
+  // Verifies our stript is running
 console.log('Connecting ...');
+
+  // Goes and tries to connect
 connect();
 
 /*
 Update the connect function to also attach an event handler to handle incoming data and console log it for the player.
 */
+
