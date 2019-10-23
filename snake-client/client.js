434dcdf01523fbe809ee569ab4d01b0fec3b2589
Ted Ogmundson <togmund@gmail.com>
2019-10-23 21:47:50 +0000

    updated the connect function in client.js to send the name of our snake to the server

--- snake-client/client.js
+++ snake-client/client.js
@@ -3,23 +3,25 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = () => {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '192.168.88.149',
     port: 50541
   });
-  
+
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   // Verify we connected
   conn.on('connect', () => {
     console.log(`I'm In.`)
-})
+    conn.write("Name: S&T");
+  })
 
   // Receive messages from the server
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
 
   // Not sure what this does!
   return conn;
