c0cd1a648b1b0a4880ebbd61f6c1c4da919f9bb0
stella-zb <upperdaystudio@gmail.com>
2019-10-23 21:59:46 +0000

    add the movement commend

--- snake-client/client.js
+++ snake-client/client.js
@@ -11,18 +11,42 @@ const connect = () => {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
-  // Verify we connected
-  conn.on('connect', () => {
-    console.log(`I'm In.`)
-    conn.write("Name: S&T");
-  })
-
   // Receive messages from the server
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
 
+    // Verify we connected
+    conn.on('connect', () => {
+      console.log(`I'm In.`)
+      conn.write("Name: S&T");
+    })
 
+    // movement function
+    // conn.on('connect', () => {
+    //   console.log(`Move Up`)
+    //   conn.write("Move: up");
+    // })
+    // conn.on('connect', () => {
+    //   console.log(`Move Left`)
+    //   conn.write("Move: left");
+    // })
+    // conn.on('connect', () => {
+    //   console.log(`Move Up`)
+    //   conn.write("Move: up");
+    // })
+    // conn.on('connect', () => {
+    //   console.log(`Move Left`)
+    //   conn.write("Move: left");
+    // })
+    // conn.on('connect', () => {
+    //   console.log(`Move Down`)
+    //   conn.write("Move: down");
+    // })
+    // conn.on('connect', () => {
+    //   console.log(`Move Right`)
+    //   conn.write("Move: right");
+    // })
   // Not sure what this does!
   return conn;
 }
