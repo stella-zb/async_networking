71876a0a9fffb583f8669dc2b326fab137b605a8
stella-zb <upperdaystudio@gmail.com>
2019-10-23 21:06:59 +0000

    create client.js file with connect function and export; motify the play.js to create dependency on client.js

---
+++ snake-client/client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = () => {
+  const conn = net.createConnection({ 
+    host: '192.168.88.149',
+    port: 50541
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  // Verify we connected
+  conn.on('connect', () => {
+    console.log(`I'm In.`)
+})
+
+  // Receive messages from the server
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  // Not sure what this does!
+  return conn;
+}
+
+module.exports = connect;
+// export the function
--- snake-client/play.js
+++ snake-client/play.js
@@ -1,38 +1,7 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = () => {
-  const conn = net.createConnection({ 
-    host: '192.168.88.149',
-    port: 50541
-  });
-  
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  // Verify we connected
-  conn.on('connect', () => {
-    console.log(`I'm In.`)
-})
-
-  // Receive messages from the server
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  // Not sure what this does!
-  return conn;
-}
-
+  // dependency on client.js
+const connect = require('./client');
   // Verifies our stript is running
 console.log('Connecting ...');
-
   // Goes and tries to connect
 connect();
-
-/*
-Update the connect function to also attach an event handler to handle incoming data and console log it for the player.
-*/
 
