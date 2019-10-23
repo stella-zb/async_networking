5fa047efbc6e59b88d7976b8a4859f57cc956906
Ted Ogmundson <togmund@gmail.com>
2019-10-23 22:43:24 +0000

    created userINput and a method of escaping the game with ctrl+c

--- snake-client/play.js
+++ snake-client/play.js
@@ -1,7 +1,30 @@
-  // dependency on client.js
+// dependency on client.js
 const connect = require('./client');
-  // Verifies our stript is running
+// Verifies our stript is running
 console.log('Connecting ...');
-  // Goes and tries to connect
-connect();
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  }
+};
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+
+// Function Calls
+// Goes and tries to connect
+connect();
+// initiallizes user inputs
+setupInput();
