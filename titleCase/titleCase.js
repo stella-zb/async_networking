e5e27db76dee5d033614994b15b31741da71a3c3
Ted Ogmundson <togmund@gmail.com>
2019-10-24 01:36:19 +0000

    added conditional for an empty input

--- titleCase/titleCase.js
+++ titleCase/titleCase.js
@@ -1,16 +1,17 @@
-const titleCase  = function(text) {
+const titleCase = (text) => {
+  if (text === "") {
+    return text
+  } else {
+    const textAsArray = text.split(" ");
+    let output = [];
+    textAsArray.forEach((element) => {
+      output.push(`${element[0].toUpperCase()}${element.slice(1).toLowerCase()}`);
+    })
+    return output.join(' ');
+  }
 
-  const textAsArray = text.split(" ");
-
-  let output = [];
-
-  textAsArray.forEach((element) => {
-    output.push(`${element[0].toUpperCase()}${element.slice(1).toLowerCase()}`);
-  })
-
-  return output.join(' ');
 };
 
-console.log(titleCase("this is an example"));
+console.log(titleCase(""));
 
 module.exports = titleCase;
