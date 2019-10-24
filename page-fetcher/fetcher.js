5650f7a84051c91a2efb8590f9c2a94fd8f18626
stella-zb <upperdaystudio@gmail.com>
2019-10-24 03:24:15 +0000

    copy over request

--- page-fetcher/fetcher.js
+++ page-fetcher/fetcher.js
@@ -1,6 +1,12 @@
 // take a URL as a command-line argument as well as a local file path and download the resource to the specified path.
-
+const request = require('request');
 const cliArgs = function () {
   return process.argv.slice(2);
+
 };
-console.log(cliArgs());
+
+request('cliArgs', (error, response, body) => {
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+});
