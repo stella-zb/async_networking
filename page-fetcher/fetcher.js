4fcf7844758b75f13acd3798f81e1867c932f9f1
Ted Ogmundson <togmund@gmail.com>
2019-10-24 03:37:47 +0000

    did some sync testing

--- page-fetcher/fetcher.js
+++ page-fetcher/fetcher.js
@@ -1,11 +1,11 @@
 // take a URL as a command-line argument as well as a local file path and download the resource to the specified path.
 /*
 Extra considerations:
-  Use the request library to make the HTTP request
-  Use Node's fs module to write the file
-  Use the callback based approach we've been learning so far
-  Do not use the pipe function
-  Do not use synchronous functions (see warning below)
+  X | Use the request library to make the HTTP request
+  O | Use Node's fs module to write the file
+  O | Use the callback based approach we've been learning so far
+  X | Do not use the pipe function
+  ? | Do not use synchronous functions (see warning below)
 */
 
 const request = require('request');
@@ -16,3 +16,4 @@ request(cliArgs[0], (error, response, body) => {
   console.log('error:', error);
   console.log('statusCode:', response && response.statusCode);
 });
+
