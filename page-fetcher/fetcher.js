7a682396753724882d8915f67c38c6aa501a0bae
Ted Ogmundson <togmund@gmail.com>
2019-10-24 03:29:42 +0000

    finished wireframe of fetcher functino which can now make a request when you type a url into terminal and log the body, error and status to the console

--- page-fetcher/fetcher.js
+++ page-fetcher/fetcher.js
@@ -1,12 +1,18 @@
 // take a URL as a command-line argument as well as a local file path and download the resource to the specified path.
+/*
+Extra considerations:
+  Use the request library to make the HTTP request
+  Use Node's fs module to write the file
+  Use the callback based approach we've been learning so far
+  Do not use the pipe function
+  Do not use synchronous functions (see warning below)
+*/
+
 const request = require('request');
-const cliArgs = function () {
-  return process.argv.slice(2);
+const cliArgs = process.argv.slice(2);
 
-};
-
-request('cliArgs', (error, response, body) => {
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+request(cliArgs[0], (error, response, body) => {
+  console.log('body:', body);
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
 });
